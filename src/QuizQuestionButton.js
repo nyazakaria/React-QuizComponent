4e628f9a15488f823d4415684f5c0709b2ef1078
NYA Zakaria <nyzakaria@icloud.com>
2022-10-01 11:30:41 +0200

    add handelEvent

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -5,10 +5,17 @@ class QuizQuestionButton  extends Component{
         super(props)
     }
 
+    handleClick() {
+        this.props.clickHandler(this.props.button_text)
+        if(this.props.button_text === this.props.quiz_question.answer){
+            this.props.showNextQuestionHandler()
+        }
+    }
+
     render() {
         return (
             <li>
-                <button>
+                <button onClick={this.handleClick.bind(this)} >
                 {this.props.button_text}
                 </button>
             </li>
